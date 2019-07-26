f389f4fce35788ed54fbb609b871e3717a44fdd4
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-26 09:33:48 -0700

    add location cards

--- src/components/LocationsList.tsx
+++ src/components/LocationsList.tsx
@@ -1,5 +1,32 @@
 import React, { useEffect, useState, ReactElement } from 'react';
+import axios from 'axios';
+import LocationCard from './LocationCard';
 
 export default function LocationsList(): ReactElement {
-  return <></>;
+  const [locations, setLocations] = useState([]);
+  const url = 'https://rickandmortyapi.com/api/location/';
+
+  useEffect((): void => {
+    async function getLocations(): Promise<void> {
+      try {
+        const result = await axios.get(url);
+        console.log(result.data.results);
+        setLocations(result.data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getLocations();
+  }, [url]);
+
+  return (
+    <section className="location-list grid-view">
+      {locations.map(
+        (location, index): ReactElement => (
+          <LocationCard key={index} location={location} />
+        )
+      )}
+    </section>
+  );
 }
